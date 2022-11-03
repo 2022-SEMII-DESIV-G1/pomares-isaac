7734fbfb234de64c4bd59b705e8c267eaf6efbcb
ipomares21 <111026257+ipomares21@users.noreply.github.com>
2022-11-02 23:41:30 -0500

    ver

--- parcial2/Script_Piramide.js
+++ parcial2/Script_Piramide.js
@@ -105,8 +105,8 @@ for (let i=0;i<Arreglo.length;i++) {
 
   document.write( "El valor de la posición i [" + i + "," + j + "] es [" + Arreglo[i][j] + "]<br/>" );
   contador1 = contador1 + Arreglo[i][j]; 
-  document.write( "El valor de la posición opuesta i [" + j + "," + i + "] es [" + Arreglo[i][j] + "]<br/>" );    
-  contador3 = contador3 + Arreglo[i][i];
+  document.write( "El valor de la posición opuesta i [" + i + "," + j + "] es [" + Arreglo[i++][j] + "]<br/>" );    
+  contador3 = contador3 + Arreglo[i++][j];
   
         for (let j=0;j<1;j++) {
         // Acceder al elemento      
