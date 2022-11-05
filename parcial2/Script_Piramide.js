eec561b6ad710bf0b5b386f6400a4490053c9c51
ipomares21 <111026257+ipomares21@users.noreply.github.com>
2022-11-04 20:00:15 -0500

    arreglo

--- parcial2/Script_Piramide.js
+++ parcial2/Script_Piramide.js
@@ -99,13 +99,15 @@ for (let i=0;i<Arreglo.length;i++) {
   
         for (let j=0;j<1;j++) {
           // Acceder al elemento  
-          document.write ( "El valor de la posición i [" + i + "," + j + "] es [" + Arreglo [i][j]+ "]<br/>" );
-          document.write( "El valor de la posición opuesta i [" + i + "," + j + "] es [" + Arreglo[j] + "]<br/>" );    
-          contador3 = contador3 + Arreglo[j];
-          contador1 = contador1 + Arreglo[i];
-          document.write( "<b>La suma del primer recorrido es [" + contador1 + "]</b><br/>" );
-          document.write( "<b>La suma segundo recorrido es [" + contador3 + "]</b><br/>" );
-
+          
         }
       }
+      document.write ( "El valor de la posición i [" + i + "," + j + "] es [" + Arreglo [i][j]+ "]<br/>" );
+      document.write( "El valor de la posición opuesta i [" + i + "," + j + "] es [" + Arreglo[j] + "]<br/>" );    
+      contador3 = contador3 + Arreglo[j];
+      contador1 = contador1 + Arreglo[i];
+      document.write( "<b>La suma del primer recorrido es [" + contador1 + "]</b><br/>" );
+      document.write( "<b>La suma segundo recorrido es [" + contador3 + "]</b><br/>" );
+
+
     }
