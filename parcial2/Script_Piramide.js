3ac67593fc7bc8dd209db1b7c6c579667feeb629
ipomares21 <111026257+ipomares21@users.noreply.github.com>
2022-11-07 21:50:58 -0500

    recorrido j

--- parcial2/Script_Piramide.js
+++ parcial2/Script_Piramide.js
@@ -1,4 +1,4 @@
-// Array
+// array
 
 let Arreglo = [
 
@@ -32,67 +32,106 @@ let Arreglo = [
 
   [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23],
 
-]
+];
 //   Llamada a la funcion calcular
 
 function Sumatot(Arreglo) {
+
   return calcular(Arreglo, 0, 0);
+
 }
 
 let contador = 0;
-const div = document.getElementById("piramide");
+
+const div = document.getElementById("Piramide");
 
 Arreglo.forEach((rows) => {
+
   contador++;
+
   const parentDiv = document.createElement("div");
+
   parentDiv.id = "parent" + contador;
+
   div.appendChild(parentDiv);
-      rows.forEach((column) => {
-        const divParent = document.getElementById("parent" + contador);
-        const childDiv = document.createElement("div");
 
-        childDiv.textContent = column;
+  rows.forEach((column) => {
 
-        divParent.appendChild(childDiv);
+    const divParent = document.getElementById("parent" + contador);
+
+    const childDiv = document.createElement("div");
+
+    childDiv.textContent = column;
+
+    divParent.appendChild(childDiv);
 
   });
-  
+
 });
 
-/*const textSum = document.getElementById("suma");
-  textSum.innerHTML = "La suma de la ruta mayor es de: " + Sumatot(Arreglo);
-  document.write( "<b>La suma del primer recorrrido es [" + Sumatot(Arreglo) + "]</b><br/>" );*/
+const textSum = document.getElementById("suma");
+
+textSum.innerHTML = "La suma de la ruta mayor es de: " + Sumatot(Arreglo);
 
  
 
+document.write( "<b>La suma del primer recorrrido es [" + Sumatot(Arreglo) + "]</b><br/>" );
 
-// calcular el arreglo en el recorrrido
 
-function calcular(Arreglo, i, j) {
-	
-  if (i == Arreglo.length) {
-    return 0;
+                // calcular el arreglo en el recorrrido
+
+                function calcular(Arreglo , i, j ) {             
+
+                  if (i == Arreglo.length) {
+                               return 0;
+
+                  }
+
+                  if (j == Arreglo.length) {
+                               return 0;
+
+                  }
+
+                  var contador1, x, y, i, j, contador2, contador3;
+
+                  contador1 = 0;
+
+                  contador2 = 0;
+
+                  contador3 = 0;
+
+ 
+
+                /*En esta bloque se realiza el primer recorrido y se realiza el calculo*/
+
+ 
+
+                for (y=0;y<Arreglo.length;y++) {
+
+                               //j=0;
+
+                               j=j+1;
+
+                               for (i=0;i<Arreglo.length;i++) {
+                       
+
+                // Acceder al elemento
+
+                  document.write( "El valor de la posición [" + i + "," + j + "] es [" + Arreglo[i][j] + "]<br/>" );
+
+                  contador1 = contador1 + Arreglo[i][j];      
+
+                  for (j<-1;j<Arreglo[i].length[j-1];j++) {
+
+
+                   }             
+               }
+
+          document.write( "<b>La suma del recorrido " +  j  + "  es ["  + contador1 + "]</b><br/>" );
+
+        contador1 = 0;
+
+                                         
+
+      }
   }
-  
-  var contador1, x, y, i, j, contador2;
-  contador1 = 0;
-  contador2 = 0;
-
-	
-	/*En esta bloque se realiza el primer recorrido y se realiza el calculo*/
-	
-	for (x=0;x<Arreglo.length;x++) {
-       for (y=0;y<Arreglo.length;y++) {
-      // Acceder al elemento
-	  
-	  document.write( "El valor de la posición [" + x + "] [" + y + "] es [" + Arreglo[x][y] + "]<br/>" );
-	  contador1 = contador1 + Arreglo[x][y];
-	  
-       }
-	   document.write( "<b>La suma del primer recorrrido es [" + contador1 + "]</b><br/>" );
-	   contador1 = 0;
-    }
-	
-	
-	document.write( "<b>La suma del primer recorrrido es [" + contador1 + "]</b><br/>" );
-}
